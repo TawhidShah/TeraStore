356b768bbc9ab5e77594b665d7aec846e4b4985e
Tawhid Shah <tawhidshahdev@gmail.com>
2024-04-28 16:31:59 +0100

    Added firebase auth integration with clerk

--- firebase.ts
+++ firebase.ts
@@ -1,14 +1,21 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCbHSoORuyscjhLSJFRcgTRrnWVrgscOs4",
+  authDomain: "terastore-12ae4.firebaseapp.com",
+  projectId: "terastore-12ae4",
+  storageBucket: "terastore-12ae4.appspot.com",
+  messagingSenderId: "149337805939",
+  appId: "1:149337805939:web:7ec9cfced0ad259d6e8606",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { db, storage };
+export { db, storage, auth };
