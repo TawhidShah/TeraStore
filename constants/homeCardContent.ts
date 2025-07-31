2fefed9dc388cdf47d427bc2dfcb111a063c58bb
Tawhid Shah <tawhidshahdev@gmail.com>
2025-07-31 21:48:48 +0100

    Homepage + header UI updates

--- constants/homeCardContent.ts
+++ constants/homeCardContent.ts
@@ -1,16 +1,24 @@
+import { ShieldCheck, Smartphone, RefreshCw } from "lucide-react";
+
 const homeCardContent = [
-    {
-        title: "Secure Storage",
-        content: "Protect what matters most. TeraStore provides ironclad security for your files, employing robust encryption protocols to keep your data safe and confidential."
-    },
-    {
-        title: "Cross-Platform Compatibility",
-        content: "Access anytime, anywhere. TeraStore seamlessly integrates with all your devices and platforms, allowing you to access your files from anywhere, regardless of the device you're using."
-    },
-    {
-        title: "Real-Time Sync",
-        content: "Stay updated, everywhere. TeraStore keeps your files synchronized in real-time across all your devices, ensuring you have the latest version at your fingertips."
-    }
+  {
+    icon: ShieldCheck,
+    title: "Secure Storage",
+    content:
+      "Protect what matters most. TeraStore provides ironclad security for your files, employing robust encryption protocols to keep your data safe and confidential.",
+  },
+  {
+    icon: Smartphone,
+    title: "Cross-Platform Compatibility",
+    content:
+      "Access anytime, anywhere. TeraStore seamlessly integrates with all your devices and platforms, allowing you to access your files from anywhere, regardless of the device you're using.",
+  },
+  {
+    icon: RefreshCw,
+    title: "Real-Time Sync",
+    content:
+      "Stay updated, everywhere. TeraStore keeps your files synchronized in real-time across all your devices, ensuring you have the latest version at your fingertips.",
+  },
 ];
 
 export default homeCardContent;
