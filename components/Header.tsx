d45a7fe541add5fe7a4987aac8f78315b03d8fac
Tawhid Shah <tawhidshahdev@gmail.com>
2025-07-31 21:48:48 +0100

    Homepage + header UI updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,9 @@ import ThemeToggler from "@/components/ThemeToggler";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border-b border-gray-300 px-6 py-3 dark:border-gray-800">
-      <Link href="/" className="flex items-center space-x-2">
-        <h1 className="text-xl font-bold">TeraStore</h1>
+    <header className="flex items-center justify-between border-b border-gray-300 h-16 px-6 dark:border-gray-800">
+      <Link href="/" className="flex items-center space-x-2 hover:scale-[101%]">
+        <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">TeraStore</h1>
       </Link>
 
       <div className="flex items-center space-x-2">
